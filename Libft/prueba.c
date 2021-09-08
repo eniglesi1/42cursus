2935984856446b398183b9ce7c1e2a99fd4dcd0b
Enmanuel Iglesias garcia del castillo <eniglesi@c1r1s6.42madrid.com>
2021-09-08 18:16:45 +0200

    Lbft_update

--- Libft/prueba.c
+++ Libft/prueba.c
@@ -18,14 +18,14 @@ int	main(void)
 {
 	int			c;
 	int			n = 50;
-	char		string[76] = "";
+	char		string[76] = "hola";
 	char		*string2;
 
 	c = 53;
 	c = c + n;
 	string2 = strdup(string);
 	printf("before = %s, %d\n", string2, c);
-	printf("%sn%s\n", ft_strdup(string), strdup(string));
+	printf("%s !\n%s !\n", ft_strdup(string), strdup(string));
 	string2 = ft_strdup(string);
 	printf("after  = %s\n", string2);
 }
