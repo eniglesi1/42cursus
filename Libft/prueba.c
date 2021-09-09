118e1834c90ae08cf805356e519e3e5b846591dd
Enmanuel Iglesias garcia del castillo <eniglesi@c1r1s1.42madrid.com>
2021-09-09 12:05:02 +0200

    Lbft_update

--- Libft/prueba.c
+++ Libft/prueba.c
@@ -13,17 +13,17 @@
 #include "libft.h"
 
 int	main(void)
-{	//declaración de variables
+{
 	int			c;
 	int			n = 50;
 	char		string[76] = "hola, que tal estás?";
 	char		*string2;
-	//asignación de variables
+	
 	c = 53;
 	c = c + n;
-	string2 = substr(string, 3, 20);											//almacena strdup de string, 3, 20 en string2 
-	printf("before = %s, %d\n", string2, c);									//imprime en pantalla el valor de string2 y de c
-	printf("%s !\n%s !\n", ft_substr(string, 7, 20), substr(string, 7, 20));	//imprime el valor de ft_substr de string, 7, 20 y el de strdup de string, 7, 20
-	string2 = ft_substr(string, 3, 20);											//almacena ft_substr de string, 3, 20 en string2
-	printf("after  = %s\n", string2);											//imprime el nuevo valor de string2
+	string2 = substr(string, 3, 20);
+	printf("before = %s, %d\n", string2, c);
+	printf("%s !\n%s !\n", ft_substr(string, 7, 20), substr(string, 7, 20));
+	string2 = ft_substr(string, 3, 20);
+	printf("after  = %s\n", string2);
 }
