edc3a0cfe693db3c559910d926ef8141986c892c
Enmanuel Iglesias garcia del castillo <eniglesi@c1r3s5.42madrid.com>
2021-10-01 19:32:17 +0200

    Lbft_update

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -10,21 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	int		aux;
+	size_t	aux;
 
 	aux = 0;
 	while (n > 0 && ft_strlen(src) > ft_strlen(dest))
 	{
 		n--;
-		dest[n] = src[n];
+		((char *)dest)[n] = ((char *)src)[n];
 	}
 	while (aux < n && ft_strlen(src) <= ft_strlen(dest))
 	{
-		dest[aux] = src[aux];
+		((char *)dest)[aux] = ((char *)src)[aux];
 		aux++;
 	}
 	return (dest);
