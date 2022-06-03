aee99863ba34c0c556a6372bb213b43ee697c274
eniglesi1 <eniglesi@student.42madrid.com>
2022-06-03 12:32:22 +0200

    Update

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -15,17 +15,43 @@
 int main(int argc, char **argv)
 {
 	int	i;
+	int	min;
+	int	max;
 	int	*a;
 	int	*b;
 
 	i = 1;
 	parseo(argc, argv);
-	a = malloc(sizeof(int) * argc);
-	b = malloc(sizeof(int) * argc);
+	a = ft_calloc(sizeof(int) * argc);
+	if (!a)
+		exit(write(1, "Malloc Error", 12) - 12);
+	b = ft_calloc(sizeof(int) * argc);
+	if (!b)
+		exit(write(1, "Malloc Error", 12) - 12);
 	while (i <= argc)
 	{
 		a[i - 1] = atoi(argv[i]);
 		i++;
 	}
+	min = minint(a);
+	max = maxint(a);
+	while (ordenados(a, b) != 1)
+	{
+		if (a[0] == min)
+			r(a);
+		while (a[0] && a[0] > a[ft_strlen(a)] && a[0] != min)
+			r(a);
+		while (a[0] && a[0] < a[ft_strlen(a)] && a[0] != min)
+			p(b);
+		if (a[0] == min)
+		{
+			r(a)
+			while(a[0] != min)
+			{
+			}
+		}
+	}
 	return (0);
 }
+2 3 4 5 84 94 920 942 2394
+304 9
