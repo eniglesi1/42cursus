a6bec9e68f02df4cbedc43ba269498f2a68d3324
Enmanuel Iglesias garcia del castillo <eniglesi@c3r3s2.42madrid.com>
2022-06-03 18:59:26 +0200

    Update

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -33,25 +33,33 @@ int main(int argc, char **argv)
 		a[i - 1] = atoi(argv[i]);
 		i++;
 	}
-	min = minint(a);
-	max = maxint(a);
-	while (ordenados(a, b) != 1)
+	while (org_ab(a, b) != 1)
 	{
-		if (a[0] == min)
-			r(a);
-		while (a[0] && a[0] > a[ft_strlen(a)] && a[0] != min)
-			r(a);
-		while (a[0] && a[0] < a[ft_strlen(a)] && a[0] != min)
-			p(b);
-		if (a[0] == min)
+		min = limits(a, 0);
+		max = limits(a, 1);
+		while (org(a, 0))
 		{
-			r(a)
-			while(a[0] != min)
-			{
-			}
+			if (a[0] == min)
+				r(a, 0);
+			while (a[0] > a[ft_strlen(a) - 1] && a[0] != min)
+				r(a, 0);
+			while (a[0] < a[ft_strlen(a) - 1] && a[0] != min)
+				p(b, 1);
+		}
+		min = limits(b, 0);
+		max = limits(b, 1);
+		while (org(b, 1))
+		{
+			if (b[0] == min)
+				r(b, 1);
+			while (b[0] > b[ft_strlen(b) - 1] && b[0] != min)
+				r(b, 1);
+			while (b[0] < b[ft_strlen(b) - 1] && b[0] != min)
+				p(a, 0);
 		}
 	}
 	return (0);
 }
 2 3 4 5 84 94 920 942 2394
-304 9
+304 9 7 15 6 25 19
+6 304
